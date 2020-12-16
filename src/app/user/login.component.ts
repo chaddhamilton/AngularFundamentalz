b864e53b6d6e3ae83bc18546862b806542ea64c3
Chadd Hamilton <chamilton@enclarapharmacia.com>
2020-12-15 19:51:38 -0500

    sad

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from './auth.service';
-
+  
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
